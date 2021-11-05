556844c3ba7741e00c57ec48bda573b4065f4495
vincent453 <93725993+vincent453@users.noreply.github.com>
2021-11-05 03:39:55 -1200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,3 @@
+var myName = "Vincent,";
+var yourName = prompt("what is your name");
+alert("My name is " + myName+"' welcome to my website " + yourName + " please sign up to play a web game" + "!");
